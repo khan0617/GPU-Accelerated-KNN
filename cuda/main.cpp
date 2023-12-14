ba131700f08428c27e750576c5ea5ea7340c3d03
Hamza Khan <hamza55014@gmail.com>
2023-12-14 10:25:15 -0600

    Add 'query_index' command line argument to c++ gpu_knn

--- cuda/main.cpp
+++ cuda/main.cpp
@@ -7,8 +7,8 @@
 #include "knn.h"
 
 /**
- * Get an array of size NUM_RECORDS * NUM_DATA_FEATURES filled with song data.
- *  To access song i's features, use: data[i * NUM_DATA_FEATURES + j]
+ * Get an array of size NUM_RECORDS * NUM_DATA_FEATURES filled with data.
+ *  To access record i's features, use: data[i * NUM_DATA_FEATURES + j]
  *  where 0 <= j < NUM_DATA_FEATURES.
  * 
  * @param filename filename of CSV file containing normalized dataset. 
@@ -21,7 +21,7 @@ float *readCSV(const std::string& filename) {
         return nullptr;
     }
 
-    // allocate one big array for the song data
+    // allocate one big array for the data
     float *data = new float[NUM_RECORDS * NUM_DATA_FEATURES];
     if (data == nullptr) {
         std::cerr << "Memory allocation failed." << std::endl;
@@ -43,10 +43,10 @@ float *readCSV(const std::string& filename) {
 }
 
 /**
- * Return a newly allocated float[NUM_DATA_FEATURES], filled with features of the song at "index".
+ * Return a newly allocated float[NUM_DATA_FEATURES], filled with features of the record at "index".
  * 
- * @param index Index of the song in the dataset. ex: if index == 5, return the array of song 5's features.
- * @param data 1D array storing song data.
+ * @param index Index of the record in the dataset. ex: if index == 5, return the array of record 5's features.
+ * @param data 1D array storing the whole dataset.
 */
 float *get_query_array(int index, float *data) {
     float *query = new float[NUM_DATA_FEATURES];
@@ -56,7 +56,20 @@ float *get_query_array(int index, float *data) {
     return query;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    // enforce user passes in query_index when calling the executable
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <query_index>" << std::endl;
+        return 1;
+    }
+
+    // bounds checking for query_index
+    int query_index = std::atoi(argv[1]);
+    if (query_index < 0 || query_index >= NUM_RECORDS) {
+        std::cerr << "Invalid query index. Must be between 0 and " << NUM_RECORDS - 1 << std::endl;
+        return 1;
+    }
+
     // read the data into a big 1D array
     float *data = readCSV(DATASET_FILENAME);
 
@@ -64,7 +77,7 @@ int main() {
         return 1;
     }
 
-    int query_index = 120000;
+    // initialize data structures
     float *query = get_query_array(query_index, data);
     float *h_distances = new float[NUM_NEIGHBORS];
     float *h_indices = new float[NUM_NEIGHBORS];
@@ -74,7 +87,7 @@ int main() {
     // print the first 10 rows of the matrix
     printf("\nFirst 10 rows of data matrix:\n");
     for (int i = 0; i < 10; ++i) {
-        printf("song %d: ", i);
+        printf("record %d: ", i);
         for (int j = 0; j < NUM_DATA_FEATURES; ++j) {
             printf("%.6f", data[i * NUM_DATA_FEATURES + j]);
             if (j < NUM_DATA_FEATURES - 1) {
